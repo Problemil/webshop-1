b28d719e744ef09feec68e922e16fccdaf439d47
Jonatan Josefsson <jonatanjosef@Jonatans-MBP.lan>
2018-01-05 13:09:46 +0100

    filtrerar specifika produkter i js fungerar

--- scripts.js
+++ scripts.js
@@ -4,9 +4,7 @@ var underKat = "";
 
 $(document).ready(function () {
 
-
     //Huvudkategori
-
     fetch("huvudkategorier.json")
         .then(function (response) {
             return response.json();
@@ -20,7 +18,7 @@ $(document).ready(function () {
                 var huvudKatMerch = huvudKat[i].title;
                 var printHuvudKat = "";
 
-                var printHuvudKat = '<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" id="merch" href="#">' + huvudKat[i].title + '<span class="caret"></span></a><ul class="dropdown-menu" id="huvudKat' + huvudKatId + '"></ul></li>';
+                var printHuvudKat = '<li class="dropdown"><a class="dropdown-toggle visaProdukter" data-toggle="dropdown" data-hover="dropdown"  id="merch" href="#">' + huvudKat[i].title + '<span class="caret"></span></a><ul class="dropdown-menu" id="huvudKat' + huvudKatId + '"></ul></li>';
                 $('#huvudKat').append(printHuvudKat);
 
             };
@@ -41,14 +39,13 @@ $(document).ready(function () {
                 var underKatHuvud = underKat[i].huvudkategori;
 
 
-                var underHuvudKat = '<li id="underKat' + underKatId + '"><a href="#">' + underKatItem + '</a></li>';
+                var underHuvudKat = '<li class="visaProdukter" data-under-kat="' + underKatId + '" id="underKat' + underKatId + '"><a href="#">' + underKatItem + '</a></li>';
 
                 if (underKatHuvud == 1) {
                     $('#huvudKat1').append(underHuvudKat);
 
                 } else if (underKatHuvud == 2) {
                     $('#huvudKat2').append(underHuvudKat);
-
 
                 } else {
                     $('#huvudKat3').append(underHuvudKat);
@@ -64,8 +61,13 @@ $(document).ready(function () {
         .then(function (data) {
             produkter = data;
 
-            $("#merch").click(function () {
+            $(".visaProdukter").click(function () {
                 var produktCardList = [];
+                var uk = $(this).data('under-kat');
+
+                $('.content').show();
+                $('.contactPage').html('');
+                $('.info').html('');
                 $("#allProducts").html("");
                 for (i = 0; i < produkter.length; i++) {
 
@@ -79,8 +81,12 @@ $(document).ready(function () {
 
                     var produktCard = '<div class="col-sm-4"><div class="card"><img class="card-img-top" src="' + produktImage + '"><div class="card-body"><h4 class="card-title">' + produktName + '</h4><p class="card-text">' + produktDesc + '</p><p>Pris: ' + produktPrice + '</p><a href="#" class="btn btn-primary">Köp nu</a></div></div></div>';
 
-                    produktCardList.push(produktCard)
+
+                    if (!uk || uk === produktUK) {
+                        produktCardList.push(produktCard);
+                    }
                 };
+
                 $('#allProducts').append(produktCardList);
             });
 
@@ -94,7 +100,6 @@ $(document).ready(function () {
     });
 
     $("#info").click(function () {
-
         $(".content").hide();
         $(".contactPage").html("<div id ='info'>Yolo</p>");
     });
